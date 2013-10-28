b07a39b859607ad7e05ed7285c54f16195c6dbb4
David Jimenez <david.jimenez19@gmail.com>
2013-10-28 12:53:33 +0000

    E2E test working
    Remove unused scripts
    add e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,9 +2,29 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('PhoneCat App', function() {
+describe('Hello world App', function() {
 
-  it('should redirect index.html to index.html#/phones', function() {
-	  browser().navigateTo('/');
-  });
+	beforeEach(function() {
+		browser().navigateTo('/');
+	});
+
+	it('should show Hello David', function() {
+		browser().navigateTo('/#/david');
+		expect(element('.hero-unit h2').text()).toMatch('Hello david');
+	});
+
+	it('should show Hello World', function() {
+		browser().navigateTo('/');
+		expect(element('.hero-unit h2').text()).toBe('Hello world');
+	});
+	
+	it('should display about page', function() {
+		browser().navigateTo('/#/about');
+		expect(element('.hero-unit p').count()).toBe(5);
+	});
+
+	it('should display contact page', function() {
+		browser().navigateTo('/#/contact');
+		expect(element('.hero-unit a').count()).toBe(4);
+	});
 });
